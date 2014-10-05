59c2b759336af07a428fd99b22d92d9ba352c262
Xingjia <xzhang009@e.ntu.edu.sg>
2014-10-05 14:52:03 +0800

    ....

--- Handler.py
+++ Handler.py
@@ -6,16 +6,28 @@ class Handler(ContentHandler,DTDHandler):
 	"""
 
 	def __init__(self):
-		self.tempData = dict()
+		self.tempData = {}
 		self.entry = ''
 		self.currentNode = ''
 		self.pubId = 0
 		self.authorId = 0
+		self.publisherId = 0
 		self.publication = ['article','inproceedings','proceedings','book','incollection','www']
 		self.fieldType = ['author','editor','title','booktitle','pages','year','address','journal','volume','number','month','url','ee','cdrom','cite','publisher','note','crossref','isbn','series','school','chapter']
 		self.authors = {}
-		self.newAuthor = False
-		# self.output = open('nodes.csv','a')
+		self.publishers = {}
+
+		self.authorFile = open('author.csv','a')
+		self.authoredFile = open('authored.csv','a')
+		self.publisherFile = open('publisher.csv','a')
+		self.publishedFile = open('published.csv','a')
+		self.articleFile = open('article.csv','a')
+		self.proceedingsFile = open('proceedings.csv','a')
+		self.inproceedingsFile = open('inproceedings.csv','a')
+		self.bookFile = open('book.csv','a')
+		self.incollectionFile = open('incollection.csv','a')
+		self.wwwFile = open('www.csv','a')
+		self.publicationFile = open('publication.csv','a')
 
 
 	def startElement(self,name,attrs):
@@ -30,49 +42,46 @@ class Handler(ContentHandler,DTDHandler):
 				print self.pubId, characters
 				if(self.authors.get(characters,-1) == -1):
 					self.authors[characters] = self.authorId
-					f = open('author.csv','a')
-					f.write(str(self.authorId)+','+characters+'\n')
-					f.close()
+					self.authorFile.write(str(self.authorId)+','+characters+'\n')
 					self.authorId+=1
 
-				f = open('authored.csv','a')
-				f.write(str(self.authors.get(characters))+','+str(self.pubId)+'\n')
-				f.close()
+				self.authoredFile.write(str(self.authors.get(characters))+','+str(self.pubId)+'\n')
+
+			if self.currentNode == 'publisher':
+				print self.pubId, characters
+				if(self.publishers.get(characters,-1) == -1):
+					self.publishers[characters] = self.publisherId
+					self.publisherFile.write(str(self.publisherId)+','+characters+'\n')
+
+					self.publisherId+=1
+
+				self.publishedFile.write(str(self.publishers.get(characters))+','+str(self.publisherId)+'\n')
+
 			self.tempData[self.currentNode] = '\"'+characters+'\"'
 
 	def endElement(self,name):	
 		self.currentNode = ''
 		if name in self.publication:
 			self.entry =  str(self.tempData.get('pubId',''))+','+self.tempData.get('pubKey','')+','+self.tempData.get('title','')+','+self.tempData.get('year','')+','+self.tempData.get('name','')+'\n'
-			f = open('publication.csv','a')
-			f.write(self.entry.encode('utf-8'))
-			f.close()
+			self.publicationFile.write(self.entry.encode('utf-8')
+
 			if name == 'article':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('journal','')+','+self.tempData.get('number','')+','+self.tempData.get('volumn','')+','+self.tempData('month','')+'\n'
-				f = open('article.csv','a')
-				f.write(self.entry.encode('utf-8'))
-				f.close()
+				self.articleFile.write(self.entry.encode('utf-8'))
 			elif name == 'inproceedings':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('booktitle','')+','+self.tempData.get('editor','')+'\n'
-				f = open('inproceedings.csv','a')
-				f.write(self.entry.encode('utf-8'))
+				self.inproceedingsFile.write(self.entry.encode('utf-8'))
 			elif name == 'proceedings':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('booktitle','')+','+self.tempData.get('editor','')+'\n'
-				f = open('proceedings.csv','a')
-				f.write(self.entry.encode('utf-8'))
+				self.proceedingsFile.write(self.entry.encode('utf-8'))
 			elif name == 'book':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('isbn','')+','+self.tempData.get('publisher','')+'\n'
-				f = open('book.csv','a')
-				f.write(self.entry.encode('utf-8'))
+				self.bookFile.write(self.entry.encode('utf-8'))
 			elif name == 'incollection':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('isbn','')+','+self.tempData.get('booktitle','')+','+self.tempData.get('publisher','')+'\n'
-				f = open('incollection.csv','a')
-				f.write(self.entry.encode('utf-8'))
+				self.incollectionFile.write(self.entry.encode('utf-8'))
 			elif name == 'www':
 				self.entry = str(self.tempData.get('pubId',''))+','+self.tempData.get('url','')+','+self.tempData.get('editor','')+'\n'
-				f = open('www.csv','a')
-				f.write(self.entry.encode('utf-8'))
-				f.close()
 
 			self.tempData = {}
 			self.pubId += 1
